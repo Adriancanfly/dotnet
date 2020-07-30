82a8c368ee02c080dd73f82d150a0f8c6224f6c7
赵琛i <W1351475237@outlook.com>
2020-07-30 20:43:24 +0800

    测试

---
+++ dotnet/A/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s=Console.ReadLine();
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ dotnet/Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=1;
+            Console.WriteLine("Hello World!");
+            
+            if(a==1)
+            {
+                Console.WriteLine("哈哈哈");
+
+            }
+            
+        }
+    }
+}
