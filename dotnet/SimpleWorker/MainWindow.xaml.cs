c9d28580159da7db0ddb46cf6b619e7e142c1329
赵琛i <W1351475237@outlook.com>
2020-07-31 19:44:41 +0800

    async\await 异步编程学习，backgroundworker类学习

---
+++ dotnet/SimpleWorker/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Threading;
+
+namespace SimpleWorker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker bgWorker = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.WorkerSupportsCancellation = true;
+
+            bgWorker.DoWork += DoWork_Handler;
+            bgWorker.ProgressChanged += ProgressChanged_Handler;
+            bgWorker.RunWorkerCompleted += RunWorkerCompleted_Handler;
+
+
+        }
+        private void ProgressChanged_Handler(object sender, ProgressChangedEventArgs args)
+        {
+            progressBar.Value = args.ProgressPercentage;
+        }
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            if (!bgWorker.IsBusy)
+                bgWorker.RunWorkerAsync();
+        }
+
+
+        private void DoWork_Handler(object sender, DoWorkEventArgs args)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (worker.CancellationPending)
+                {
+                    args.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    worker.ReportProgress(i * 10);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        private void RunWorkerCompleted_Handler(object sender, RunWorkerCompletedEventArgs args)
+        {
+            progressBar.Value = 0;
+
+            if (args.Cancelled)
+                MessageBox.Show("Process was cancelled,", "Process Cancelled");
+            else
+                MessageBox.Show("Process completed normally.", "Process Completed");
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            bgWorker.CancelAsync();
+        }
+    }
+}
--- dotnet/project1/Program.cs
+++ dotnet/project1/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-namespace B
+namespace project1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
         }
     }
