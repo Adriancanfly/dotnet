1af785a86474594a157dae4201a000386ab9b038
赵琛i <W1351475237@outlook.com>
2020-08-01 13:59:14 +0800

    vscode 引用其他程序集的一种方法

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+        public int a=10;
+        public int b=10;
+
+        public static int Sum(Class1 c)
+        {
+            return c.a+c.b;
+        } 
+    }
+}
