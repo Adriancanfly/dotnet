944c69c5e4a7ba4209acea3e99cab6b2bd51d82e
赵琛i <W1351475237@outlook.com>
2020-08-01 14:58:50 +0800

    Exception 异常处理try catch finally

---
+++ Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+
+            try
+            {
+                int y = 0;
+                x /= y;
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Message: {0}",e.Message);
+                Console.WriteLine("Source: {0}",e.Source);
+                Console.WriteLine("StackTrace: {0}",e.StackTrace);
+                Console.WriteLine("请重新输入");
+                int y = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{x} / {y} = {x / y}");
+                Console.WriteLine("Handling all exceptions - Keep on Running");
+            }
+        }
+    }
+}
